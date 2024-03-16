68571a9b3d4491ac93a711a886d06d249331b2b3
cristian6425 <cristianchacondt@gmail.com>
2024-03-16 18:31:40 -0500

    add ios validation

--- lib/flutter_automatic_time.dart
+++ lib/flutter_automatic_time.dart
@@ -1,8 +1,14 @@
 
+import 'dart:io';
+
 import 'flutter_automatic_time_platform_interface.dart';
 
 class FlutterAutomaticTime {
   Future<bool?> isAutomaticTime() {
-    return FlutterAutomaticTimePlatform.instance.isAutomaticTime();
+    if (Platform.isAndroid){
+      return FlutterAutomaticTimePlatform.instance.isAutomaticTime();
+    }else{
+      return Future.value(true);
+    }
   }
 }
